b121c60055f858535e4e2ccaefcae98b65b10b0e
mccannecology <michael.mccann@stonybrook.edu>
2014-02-28 14:44:06 -0500

    corrected input file name

--- RUNITALL.R
+++ RUNITALL.R
@@ -37,7 +37,7 @@
 # 
 ########################################################################################################
 for (i in 1:31) { # loop through all of your simulations - User needs to specify the max # of simulations (rows of parameters) in .csv
-  parameters <- read.csv("input02.csv") # imports parameter  values for all simulations 
+  parameters <- read.csv("input03.csv") # imports parameter  values for all simulations 
   
   require(R.utils) # package for sourceDirectory()
   
@@ -72,7 +72,7 @@ for (i in 1:31) { # loop through all of your simulations - User needs to specify
   parameters$propyears_propdaysFP_abovehalf <- NA # set-up a blank column 
   parameters$propyears_propdaysFP_abovehalf[simulnumb] <- propyears_propdaysFP_abovehalf # assign the current simulations results to the correct spot
   
-  write.csv(parameters,"input02.csv",append=T,row.names=F) # add these results to your original input file 
+  write.csv(parameters,"input03.csv",append=T,row.names=F) # add these results to your original input file 
     
   rm(list=ls()) # clear workspace for next simulation 
   
