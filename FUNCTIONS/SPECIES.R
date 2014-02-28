34f85dc3821e4458f6e25df54f162b3f69b77bf1
mccannecology <mccannmikejames@gmail.com>
2014-02-28 10:10:19 -0500

    ACTUALLY fills speciesmatrix with the species-specific values (instead of repeating the 1st species)

--- FUNCTIONS/SPECIES.R
+++ FUNCTIONS/SPECIES.R
@@ -7,20 +7,22 @@
 ##########################################
 
 SPECIES <- function(x){  
-  simulnumb <- x 
+  parameters <- read.csv("input02.csv")
+  
+  simulnumb <- x   
   
   # make the number of columns = #variables + 1 
-  speciesmatrix <- matrix(data=0,nrow=parameters[simulnumb,11],ncol=5)
+  speciesmatrix <- matrix(data=0,nrow=parameters$numbspecies[simulnumb],ncol=5)
   speciesmatrix <- as.data.frame(speciesmatrix)
   colnames(speciesmatrix)<-c("species","maxrgr","agedead","initial","overwinter")
   
   # build a species matrix from the parameter values defined elsewhere
-  speciesmatrix[1]<-seq(1,numbspecies,1)    
-  speciesmatrix[2]<-as.vector(parameters[simulnumb,20]:parameters[simulnumb,20+numbspecies-1])
-  speciesmatrix[3]<-as.vector(parameters[simulnumb,16]:parameters[simulnumb,16+numbspecies-1])
-  speciesmatrix[4]<-as.vector(parameters[simulnumb,12]:parameters[simulnumb,12+numbspecies-1])
-  speciesmatrix[5]<-as.vector(parameters[simulnumb,24]:parameters[simulnumb,24+numbspecies-1])
-  
+  speciesmatrix[1]<-seq(1,numbspecies,1) # a vector of species numbers 
+  for (i in 1:numbspecies) {speciesmatrix[i,2] <- parameters[simulnumb,19+i]} # a vector of maxrgr - length varies on number of species
+  for (i in 1:numbspecies) {speciesmatrix[i,3] <- parameters[simulnumb,15+i]} # a vector of agedead - length varies on number of species
+  for (i in 1:numbspecies) {speciesmatrix[i,4] <- parameters[simulnumb,11+i]} # a vector of initial - length varies on number of species
+  for (i in 1:numbspecies) {speciesmatrix[i,5] <- parameters[simulnumb,23+i]} # a vector of overwinter - length varies on number of species
+    
   assign("speciesmatrix",speciesmatrix, envir = .GlobalEnv)
   
 } 
