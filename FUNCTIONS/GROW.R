05e87820ce1f0c425e8fafba0e9360d4d00ead5e
mccannecology <mccannmikejames@gmail.com>
2014-03-23 15:20:18 -0400

    annotation

--- FUNCTIONS/GROW.R
+++ FUNCTIONS/GROW.R
@@ -6,9 +6,9 @@ GROW <- function(x1,x2,x3) { # function has LIST[[i]]$SPmatrix, LIST[[i+1]]$SPma
   for (j in 1:height) { # loop over all rows (height)
     for (k in 1:width) { # loop over all columns (width)
       if (x1[j,k] > 0) {
-        x2[j,k] <- x1[j,k] + maxrgr*x1[j,k] * # initial biomass plus new growtht 
+        x2[j,k] <- x1[j,k] + maxrgr*x1[j,k] * # initial biomass plus new growth 
                       
-                      (halfsatB/(x3[j,k]+halfsatB)) * # biomass limitation ** KEY CHANGE: replace x1[j,k] w/ a multispecies measure
+                      (halfsatB/(x3[j,k]+halfsatB)) * # biomass limitation 
                       
                       ((TOTALP/(TOTALP+halfsatP))*(TOTALN/(TOTALN+halfsatN))) - # nutrient limitation 
           
