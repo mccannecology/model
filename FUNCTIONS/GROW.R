d0d376b1b37885669de1462c260673d0f3d39ae6
mccannecology <mccannmikejames@gmail.com>
2014-03-23 12:32:29 -0400

    both GROW and STEP will read SPALLmatrix for biomass limitation of growth

--- FUNCTIONS/GROW.R
+++ FUNCTIONS/GROW.R
@@ -1,10 +1,10 @@
-GROW <- function(x1,x2) { # function has both LIST[[i]]$SPmatrix and LIST[[i+1]]$SPmatrix as inputs 
+GROW <- function(x1,x2,x3) { # function has both LIST[[i]]$SPmatrix and LIST[[i+1]]$SPmatrix as inputs 
   for (j in 1:height) { # loop over all rows (height)
     for (k in 1:width) { # loop over all columns (width)
       if (x1[j,k] > 0) {
         x2[j,k] <- x1[j,k] + maxrgr*x1[j,k] * # initial biomass plus new growtht 
                       
-                      (halfsatB/(x1[j,k]+halfsatB)) * # biomass limitation ** KEY CHANGE: replace x1[j,k] w/ a multispecies measure
+                      (halfsatB/(x3[j,k]+halfsatB)) * # biomass limitation ** KEY CHANGE: replace x1[j,k] w/ a multispecies measure
                       
                       ((TOTALP/(TOTALP+halfsatP))*(TOTALN/(TOTALN+halfsatN))) - # nutrient limitation 
           
--- STEP10.R
+++ STEP10.R
@@ -7,25 +7,25 @@ STEP10 <- function() { # start defining the function
     if (i %ni% winters) { 
     
       if (numbspecies == 4) { 
-        LIST[[i+1]]$SP4matrix <- GROW(LIST[[i]]$SP4matrix, LIST[[i+1]]$SP4matrix) 
-        LIST[[i+1]]$SP3matrix <- GROW(LIST[[i]]$SP3matrix, LIST[[i+1]]$SP3matrix) 
-        LIST[[i+1]]$SP2matrix <- GROW(LIST[[i]]$SP2matrix, LIST[[i+1]]$SP2matrix) 
-        LIST[[i+1]]$SP1matrix <- GROW(LIST[[i]]$SP1matrix, LIST[[i+1]]$SP1matrix)
+        LIST[[i+1]]$SP4matrix <- GROW(LIST[[i]]$SP4matrix, LIST[[i+1]]$SP4matrix, LIST[[i]]$SPALLmatrix) 
+        LIST[[i+1]]$SP3matrix <- GROW(LIST[[i]]$SP3matrix, LIST[[i+1]]$SP3matrix, LIST[[i]]$SPALLmatrix) 
+        LIST[[i+1]]$SP2matrix <- GROW(LIST[[i]]$SP2matrix, LIST[[i+1]]$SP2matrix, LIST[[i]]$SPALLmatrix) 
+        LIST[[i+1]]$SP1matrix <- GROW(LIST[[i]]$SP1matrix, LIST[[i+1]]$SP1matrix, LIST[[i]]$SPALLmatrix)
         LIST[[i+1]]$SPALLmatrix <- LIST[[i+1]]$SP4matrix + LIST[[i+1]]$SP3matrix + LIST[[i+1]]$SP2matrix + LIST[[i+1]]$SP1matrix
       }
       else if (numbspecies == 3) {
-        LIST[[i+1]]$SP3matrix <- GROW(LIST[[i]]$SP3matrix, LIST[[i+1]]$SP3matrix) 
-        LIST[[i+1]]$SP2matrix <- GROW(LIST[[i]]$SP2matrix, LIST[[i+1]]$SP2matrix) 
-        LIST[[i+1]]$SP1matrix <- GROW(LIST[[i]]$SP1matrix, LIST[[i+1]]$SP1matrix) 
+        LIST[[i+1]]$SP3matrix <- GROW(LIST[[i]]$SP3matrix, LIST[[i+1]]$SP3matrix, LIST[[i]]$SPALLmatrix) 
+        LIST[[i+1]]$SP2matrix <- GROW(LIST[[i]]$SP2matrix, LIST[[i+1]]$SP2matrix, LIST[[i]]$SPALLmatrix) 
+        LIST[[i+1]]$SP1matrix <- GROW(LIST[[i]]$SP1matrix, LIST[[i+1]]$SP1matrix, LIST[[i]]$SPALLmatrix) 
         LIST[[i+1]]$SPALLmatrix <- LIST[[i+1]]$SP3matrix + LIST[[i+1]]$SP2matrix + LIST[[i+1]]$SP1matrix
       }
       else if (numbspecies == 2) {
-        LIST[[i+1]]$SP2matrix <- GROW(LIST[[i]]$SP2matrix, LIST[[i+1]]$SP2matrix) 
-        LIST[[i+1]]$SP1matrix <- GROW(LIST[[i]]$SP1matrix, LIST[[i+1]]$SP1matrix) 
-        LIST[[i+1]]$SPALLmatrix <- LIST[[i+1]]$SP2matrix + LIST[[i+1]]$SP1matrix
+        LIST[[i+1]]$SP2matrix <- GROW(LIST[[i]]$SP2matrix, LIST[[i+1]]$SP2matrix, LIST[[i]]$SPALLmatrix) 
+        LIST[[i+1]]$SP1matrix <- GROW(LIST[[i]]$SP1matrix, LIST[[i+1]]$SP1matrix, LIST[[i]]$SPALLmatrix) 
+        LIST[[i+1]]$SPALLmatrix <- LIST[[i+1]]$SP2matrix + LIST[[i+1]]$SP1matrix, LIST[[i]]$SPALLmatrix)
       }
       else if (numbspecies == 1) {
-        LIST[[i+1]]$SP1matrix <- GROW(LIST[[i]]$SP1matrix, LIST[[i+1]]$SP1matrix) 
+        LIST[[i+1]]$SP1matrix <- GROW(LIST[[i]]$SP1matrix, LIST[[i+1]]$SP1matrix, LIST[[i]]$SPALLmatrix) 
         LIST[[i+1]]$SPALLmatrix <- LIST[[i+1]]$SP1matrix
       }
     
