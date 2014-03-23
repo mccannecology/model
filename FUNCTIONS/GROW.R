4bb08a7eaad59cd49f37dac7b6454380650eba37
mccannecology <mccannmikejames@gmail.com>
2014-03-23 18:57:33 -0400

    now a function of species-specific speciesmatrix

--- FUNCTIONS/GROW.R
+++ FUNCTIONS/GROW.R
@@ -6,19 +6,20 @@
 # x1... LIST[[i]]$SPmatrix            #
 # x2... LIST[[i+1]]$SPmatrix          #
 # x3... LIST[[i]]$SPALLmatrix         #
+# n... species # 
 #                                     #
-# Created: MJ McCann 3/22/2013        #
+# Created: MJ McCann 3/23/2013        #
 #######################################
 
-GROW <- function(x1,x2,x3) { # function has LIST[[i]]$SPmatrix, LIST[[i+1]]$SPmatrix, and LIST[[i]]$SPALLmatrix as inputs 
+GROW <- function(x1,x2,x3,n) { 
   for (j in 1:height) { # loop over all rows (height)
     for (k in 1:width) { # loop over all columns (width)
       if (x1[j,k] > 0) {
-        x2[j,k] <- x1[j,k] + maxrgr*x1[j,k] * # initial biomass plus new growth 
+        x2[j,k] <- x1[j,k] + speciesmatrix$maxrgr[n]*x1[j,k] * # initial biomass plus new growth 
                       
-                      (halfsatB/(x3[j,k]+halfsatB)) * # biomass limitation 
+                      (speciesmatrix$halfsatB[n]/(x3[j,k]+speciesmatrix$halfsatB[n])) * # biomass limitation 
                       
-                      ((TOTALP/(TOTALP+halfsatP))*(TOTALN/(TOTALN+halfsatN))) - # nutrient limitation 
+                      ((TOTALP/(TOTALP+speciesmatrix$halfsatP[n]))*(TOTALN/(TOTALN+speciesmatrix$halfsatN[n]))) - # nutrient limitation 
           
                       (loss*x1[j,k]) # biomass loss 
       }  
