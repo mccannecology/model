f2996c731483df5b5c11e8f6846f47e995955cd9
mccannecology <mccannmikejames@gmail.com>
2014-03-23 12:19:08 -0400

    now correctly builds the SPALLmatrix

--- FUNCTIONS/START4.R
+++ FUNCTIONS/START4.R
@@ -14,15 +14,24 @@ START4 <- function(){
   # issue a warning message if you try to create too many initial individuals (i.e., more than # of spots)
   if (sum(speciesmatrix$initial) > (height*width)) {warning("Too many initial individuals for this pond!")}
   
-  #######################################
-  # Build presence/absence matrix 
-  #######################################
+  ####################################################
+  # Build presence/absence matrices for each species 
+  ####################################################
   # loop through # of species you have 
   mylist<-list()
   
-  for (n in 1:numbspecies+1) { 
+  for (n in 1:numbspecies) { 
     initial<-c(rep(0,((height*width)-speciesmatrix[n,"initial"])),rep(1,speciesmatrix[n,"initial"])) # make a vector of 0s or 1s depending on your initial # from the species matrix     
     mylist[[n]] <- matrix(sample(initial),height,width) # randomly assign the elements of this vector to the SPmatrix without replacement 
+  }
+  
+  ####################################################
+  # Build SPALLmatrix - sum of all species 
+  ####################################################
+  SPALLmatrix <- matrix(0,height,width)  
+  
+  for (n in 1:numbspecies) {
+    SPALLmatrix <- SPALLmatrix + mylist[[n]]
   }
   
   #######################################
@@ -44,7 +53,7 @@ START4 <- function(){
   #######################################
   # Put it all together & return it 
   #######################################
-  mylist <- c(mylist, list(TOTALN), list(TOTALP))
+  mylist <- c(mylist, list(SPALLmatrix), list(TOTALN), list(TOTALP))
   
   names(mylist) <- c(paste("SP",seq(from=1,to=numbspecies,by=1),"matrix",sep=""),"SPALLmatrix","TOTALN","TOTALP")
   
