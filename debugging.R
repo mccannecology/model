798ea04b408a527827b66d7d1053d3066a5cb4e8
mccannecology <mccannmikejames@gmail.com>
2014-03-23 11:02:35 -0400

    adding testing of GROW()

--- debugging.R
+++ debugging.R
@@ -21,3 +21,22 @@ LIST[[2]]$SP3matrix <- GROW(LIST[[1]]$SP2matrix, LIST[[2]]$SP2matrix)
 
 # Test function STEP10() on the LIST 
 LIST<-STEP10()
+
+
+# playing around with some forms of the growth formula for GROW()
+
+# define parameters 
+# will use speciesmatrix indexing in the future 
+maxrgr<-0.4
+halfsatP<-0.05
+halfsatN<-0.04
+halfsatB<-42
+TOTALN<-2
+TOTALP<-0.5
+loss<-0.05
+
+LIST[[1]]$SP1matrix
+LIST[[1]]$SP1matrix[10,2]
+LIST[[1]]$SP1matrix[10,2] + maxrgr*LIST[[1]]$SP1matrix[10,2]*(halfsatB/(LIST[[1]]$SP1matrix[10,2]+halfsatB))*
+  ((TOTALP/(TOTALP+halfsatP))*(TOTALN/(TOTALN+halfsatN))) -
+  (loss*LIST[[1]]$SP1matrix[10,2])
