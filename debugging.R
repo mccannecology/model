6144c9295b81cc65159b40d83e91518588b80ab1
mccannecology <mccannmikejames@gmail.com>
2014-03-23 10:25:46 -0400

    script to generate test LISTS and test functions

---
+++ debugging.R
@@ -0,0 +1,20 @@
+#############################################################################################
+# Make some examples LISTs
+# occurs in RUNITALL in the real thing
+
+# Create a blank LIST 
+LIST <- vector("list",10) # Creates the "blank" LIST 
+
+# fill  the LIST w/ matrices of 0 
+for (i in 1:10){ # 
+  LIST[[i]] <- BLANK2()
+}
+
+# Start the first time step with some individuals 
+LIST[[1]]<-START4() 
+#############################################################################################
+
+# Testing the function GROW()
+LIST[[2]]$SP1matrix <- GROW(LIST[[1]]$SP1matrix, LIST[[2]]$SP1matrix) 
+LIST[[2]]$SP2matrix <- GROW(LIST[[1]]$SP2matrix, LIST[[2]]$SP2matrix) 
+LIST[[2]]$SP3matrix <- GROW(LIST[[1]]$SP2matrix, LIST[[2]]$SP2matrix) 
