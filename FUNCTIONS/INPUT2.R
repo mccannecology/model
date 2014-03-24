fc156b2df7ab1d7e61d5e710d14a25585e97cdc2
mccannecology <mccannmikejames@gmail.com>
2014-03-24 08:10:31 -0400

    now compatible with new parameters associated w/ new LIST structure

--- FUNCTIONS/INPUT2.R
+++ FUNCTIONS/INPUT2.R
@@ -1,10 +1,10 @@
 #####################################################
 # Accepts .csv of parameter values                  #
 # Assigns values to objects in environment          #
-# STILL IN DEVELOPMENT                              #
+# Compatible with new LIST structure                #
 #                                                   #
 # By: Michael J. McCann                             #
-# Last Updated: 2/26/2014                           #
+# Major revision: 3/24/2014                         #
 ##################################################### 
 INPUT <- function(x){  
   assign("height", parameters$height[simulnumb], envir = .GlobalEnv)  # height of the grid
@@ -13,23 +13,13 @@ INPUT <- function(x){
   assign("timesteps", parameters$timesteps[simulnumb], envir = .GlobalEnv)  # number of time-steps (+1, actually) in a "growing season" 
   assign("years", parameters$years[simulnumb], envir = .GlobalEnv)      # number of years ("growing seasons") to run the model 
   
-  assign("wind_prob", parameters$wind_prob[simulnumb], envir = .GlobalEnv)  # proportion of time steps where wind knocks a row/col off of the grid 
-  assign("wind_directions", c(as.character(parameters$wind_directions[simulnumb]),as.character(parameters$wind_directions.1[simulnumb]),as.character(parameters$wind_directions.2[simulnumb]),as.character(parameters$wind_directions.3[simulnumb])), envir = .GlobalEnv)
-  
-  assign("buffer", parameters$buffer[simulnumb], envir = .GlobalEnv)    # distance from focal cell - used to count up the number of neighbors 
-  
   assign("numbspecies", parameters$numbspecies[simulnumb], envir = .GlobalEnv) # number of species in the model 
   
   assign("initial01", parameters$initial01[simulnumb], envir = .GlobalEnv)   # initial number of individuals - species 01
   assign("initial02", parameters$initial02[simulnumb], envir = .GlobalEnv)   # initial number of individuals - species 02
   assign("initial03", parameters$initial03[simulnumb], envir = .GlobalEnv)   # initial number of individuals - species 03
   assign("initial04", parameters$initial04[simulnumb], envir = .GlobalEnv)   # initial number of individuals - species 04
-  
-  assign("agedead01", parameters$agedead01[simulnumb], envir = .GlobalEnv)   # average age that individuals die at - species 01
-  assign("agedead02", parameters$agedead02[simulnumb], envir = .GlobalEnv)   # average age that individuals die at - species 02
-  assign("agedead03", parameters$agedead03[simulnumb], envir = .GlobalEnv)   # average age that individuals die at - species 03
-  assign("agedead04", parameters$agedead04[simulnumb], envir = .GlobalEnv)   # average age that individuals die at - species 04
-  
+    
   assign("maxrgr01", parameters$maxrgr01[simulnumb], envir = .GlobalEnv)    # maximum relative growth rate - species 01
   assign("maxrgr02", parameters$maxrgr02[simulnumb], envir = .GlobalEnv)    # maximum relative growth rate - species 02
   assign("maxrgr03", parameters$maxrgr03[simulnumb], envir = .GlobalEnv)    # maximum relative growth rate - species 03
@@ -39,4 +29,29 @@ INPUT <- function(x){
   assign("overwinter02", parameters$overwinter02[simulnumb], envir = .GlobalEnv)    # proportion of individuals that overwinter - species 02
   assign("overwinter03", parameters$overwinter03[simulnumb], envir = .GlobalEnv)    # proportion of individuals that overwinter - species 03  
   assign("overwinter04", parameters$overwinter04[simulnumb], envir = .GlobalEnv)    # proportion of individuals that overwinter - species 04
+  
+  assign("halfsatB01", parameters$halfsatB01[simulnumb], envir = .GlobalEnv)    # biomass half saturation - species 01  
+  assign("halfsatB02", parameters$halfsatB02[simulnumb], envir = .GlobalEnv)    # biomass half saturation - species 02
+  assign("halfsatB03", parameters$halfsatB03[simulnumb], envir = .GlobalEnv)    # biomass half saturation - species 03  
+  assign("halfsatB04", parameters$halfsatB04[simulnumb], envir = .GlobalEnv)    # biomass half saturation - species 04
+  
+  assign("halfsatN01", parameters$halfsatN01[simulnumb], envir = .GlobalEnv)    # nitrogen half saturation - species 01  
+  assign("halfsatN02", parameters$halfsatN02[simulnumb], envir = .GlobalEnv)    # nitrogen half saturation - species 02
+  assign("halfsatN03", parameters$halfsatN03[simulnumb], envir = .GlobalEnv)    # nitrogen half saturation - species 03  
+  assign("halfsatN04", parameters$halfsatN04[simulnumb], envir = .GlobalEnv)    # nitrogen half saturation - species 04
+  
+  assign("halfsatP01", parameters$halfsatP01[simulnumb], envir = .GlobalEnv)    # phosphorus half saturation - species 01  
+  assign("halfsatP02", parameters$halfsatP02[simulnumb], envir = .GlobalEnv)    # phosphorus half saturation - species 02
+  assign("halfsatP03", parameters$halfsatP03[simulnumb], envir = .GlobalEnv)    # phosphorus half saturation - species 03  
+  assign("halfsatP04", parameters$halfsatP04[simulnumb], envir = .GlobalEnv)    # phosphorus half saturation - species 04
+  
+  assign("uptakeN01", parameters$uptakeN01[simulnumb], envir = .GlobalEnv)    # nitrogen uptake rate - species 01  
+  assign("uptakeN02", parameters$uptakeN02[simulnumb], envir = .GlobalEnv)    # nitrogen uptake rate - species 02
+  assign("uptakeN03", parameters$uptakeN03[simulnumb], envir = .GlobalEnv)    # nitrogen uptake rate - species 03  
+  assign("uptakeN04", parameters$uptakeN04[simulnumb], envir = .GlobalEnv)    # nitrogen uptake rate - species 04
+  
+  assign("uptakeP01", parameters$uptakeP01[simulnumb], envir = .GlobalEnv)    # phosphorus uptake rate - species 01  
+  assign("uptakeP02", parameters$uptakeP02[simulnumb], envir = .GlobalEnv)    # phosphorus uptake rate - species 02
+  assign("uptakeP03", parameters$uptakeP03[simulnumb], envir = .GlobalEnv)    # phosphorus uptake rate - species 03  
+  assign("uptakeP04", parameters$uptakeP04[simulnumb], envir = .GlobalEnv)    # phosphorus uptake rate - species 04
 }
--- FUNCTIONS/SPECIES2.R
+++ FUNCTIONS/SPECIES2.R
@@ -1,24 +1,29 @@
 ##########################################
 # Generates species matrix               #
 # Used to loop through species           #
+# Compatible with new LIST structure     #
 #                                        #  
 # By: Michael J. McCann                  #
-# Last Updated: 2/26/2014                #
+# Major revision: 3/24/2014              #
 ##########################################
 
 SPECIES <- function(x){  
   # make the number of columns = #variables + 1 
-  speciesmatrix <- matrix(data=0,nrow=parameters$numbspecies[simulnumb],ncol=5)
+  speciesmatrix <- matrix(data=0,nrow=parameters$numbspecies[simulnumb],ncol=9)
   speciesmatrix <- as.data.frame(speciesmatrix)
-  colnames(speciesmatrix)<-c("species","maxrgr","agedead","initial","overwinter")
+  colnames(speciesmatrix)<-c("species","maxrgr","initial","overwinter","halfsatB","halfsatN","halfsatP","uptakeN","uptakeP")
   
   # build a species matrix from the parameter values defined elsewhere
   speciesmatrix[1]<-seq(1,numbspecies,1) # a vector of species numbers 
-  for (i in 1:numbspecies) {speciesmatrix[i,2] <- parameters[simulnumb,19+i]} # a vector of maxrgr - length varies on number of species
-  for (i in 1:numbspecies) {speciesmatrix[i,3] <- parameters[simulnumb,15+i]} # a vector of agedead - length varies on number of species
-  for (i in 1:numbspecies) {speciesmatrix[i,4] <- parameters[simulnumb,11+i]} # a vector of initial - length varies on number of species
-  for (i in 1:numbspecies) {speciesmatrix[i,5] <- parameters[simulnumb,23+i]} # a vector of overwinter - length varies on number of species
-    
+  for (i in 1:numbspecies) {speciesmatrix[i,2] <- parameters[simulnumb,10+i]} # a vector of maxrgr - length varies on number of species
+  for (i in 1:numbspecies) {speciesmatrix[i,3] <- parameters[simulnumb,6+i]} # a vector of initial - length varies on number of species
+  for (i in 1:numbspecies) {speciesmatrix[i,4] <- parameters[simulnumb,14+i]} # a vector of overwinter - length varies on number of species
+  for (i in 1:numbspecies) {speciesmatrix[i,5] <- parameters[simulnumb,18+i]} # a vector of halfsatB - length varies on number of species
+  for (i in 1:numbspecies) {speciesmatrix[i,6] <- parameters[simulnumb,22+i]} # a vector of halfsatN - length varies on number of species
+  for (i in 1:numbspecies) {speciesmatrix[i,7] <- parameters[simulnumb,26+i]} # a vector of halfsatP - length varies on number of species
+  for (i in 1:numbspecies) {speciesmatrix[i,8] <- parameters[simulnumb,30+i]} # a vector of uptakeN - length varies on number of species
+  for (i in 1:numbspecies) {speciesmatrix[i,9] <- parameters[simulnumb,34+i]} # a vector of uptakeP - length varies on number of species
+  
   assign("speciesmatrix",speciesmatrix, envir = .GlobalEnv)
   
 } 
