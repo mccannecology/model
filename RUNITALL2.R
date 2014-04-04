1fc55d2888e3deb066c8b8ef1cad0741fb5cc9dc
mccannecology <mccannmikejames@gmail.com>
2014-04-04 09:26:48 -0400

    set up to run more simulations

--- RUNITALL2.R
+++ RUNITALL2.R
@@ -29,14 +29,14 @@
 # 
 ########################################################################################################
 # Enter your total number of simulations - should be same as # rows in "inputXX.csv"
-totalsimuls <- 17 
+totalsimuls <- 168 
 
 # set-up blank vectors for any of the results 
 propyears_avgFPcover_abovethreshold <- rep(NA, totalsimuls)
 propyears_prop_daysFP_abovehalf <- rep(NA, totalsimuls)
 RESULTS <- data.frame(propyears_avgFPcover_abovethreshold,propyears_prop_daysFP_abovehalf)
 
-parameters <- read.csv("input02.csv") # imports parameter  values for all simulations 
+parameters <- read.csv("input03.csv") # imports parameter  values for all simulations 
 
 for (i in 1:totalsimuls) { # loop through all of your simulations - User needs to specify the max # of simulations (rows of parameters) in .csv
   require(R.utils) # package for sourceDirectory()
@@ -80,5 +80,5 @@ parameters$propyears_avgFPcover_abovethreshold <- RESULTS[,1]
 parameters$propyears_prop_daysFP_abovehalf <- RESULTS[,2]
 
 # add these results to your original input file and write as a .csv 
-write.csv(parameters,"output02.csv",row.names=F) 
+write.csv(parameters,"output03.csv",row.names=F) 
 
