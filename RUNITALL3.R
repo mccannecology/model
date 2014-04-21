086d26c2c9489dcc89b85b3195c8b1c654ece5eb
mccannecology <michael.mccann@stonybrook.edu>
2014-04-21 13:51:42 -0400

    changes to get foreach loop to work

--- RUNITALL3.R
+++ RUNITALL3.R
@@ -29,43 +29,53 @@
 # 
 ########################################################################################################
 # Enter your total number of simulations - should be same as # rows in "inputXX.csv"
-totalsimuls <- 96
+totalsimuls <- 3
 
-# set-up blank vectors for any of the results 
-propyears_avgFPcover_abovethreshold <- rep(NA, totalsimuls)
-propyears_prop_daysFP_abovehalf <- rep(NA, totalsimuls)
-RESULTS <- data.frame(propyears_avgFPcover_abovethreshold,propyears_prop_daysFP_abovehalf)
+# set-up blank vectors for each of the results 
+# propyears_avgFPcover_abovethreshold <- rep(NA, totalsimuls) # The proportion of years where the average FP cover is greater than "threshold" (defined as input to OUTPUT())
+# propyears_prop_daysFP_abovehalf <- rep(NA, totalsimuls) # The proportion of years where >50% days are reater than "threshold" FP cover (defined as input to OUTPUT())
+# avg_avg_FPcover <- rep(NA, totalsimuls) # The proportion of years where >50% days are greater than "threshold" FP cover (defined as input to OUTPUT())
 
-parameters <- read.csv("input04.csv") # imports parameter  values for all simulations 
+# combine all of those different results into a data frame (currently blank)
+# RESULTS <- data.frame(propyears_avgFPcover_abovethreshold,propyears_prop_daysFP_abovehalf,avg_avg_FPcover)
+
+# imports parameter  values for all simulations 
+parameters <- read.csv("input05.csv") 
+
+parameters$propyears_avgFPcover_abovethreshold <- rep(NA, totalsimuls)
+parameters$propyears_prop_daysFP_abovehalf <- rep(NA, totalsimuls)
+parameters$avg_avg_FPcover <- rep(NA, totalsimuls)
 
 require(foreach)
 require(doSNOW)
-require(parallel)
-cl <- makeCluster(3,"PSOCK") # not sure if I should set this to 4
+
+cl <- makeCluster(3,"SOCK") 
 
 require(R.utils) # package for sourceDirectory()
 
 sourceDirectory(path=paste(getwd(),"/FUNCTIONS",sep=""),recursive=FALSE) # load all your functions
 
+#  assigns the functions to the global environments of each node
 clusterExport(cl, c("BLANK2", "GROW", "INPUT2","MOVE",
                     "OUTPUT2","OVERWINTER","RELEASE_N","RELEASE_P",
                     "SPECIES2","START4","STEP10","UPTAKE_N","UPTAKE_P"))
 
-registerDoSNOW(cl)
-getDoParWorkers() 
+registerDoSNOW(cl) # registers the SNOW parallel backend w/ foreach package 
+getDoParWorkers() # returns the # of workers 
 
-foreach (i=1:totalsimuls) %dopar% { # loop through all of your simulations - User needs to specify the max # of simulations (rows of parameters) in .csv
-  #require(R.utils) # package for sourceDirectory()
+foreach (i=1:totalsimuls, .foreach=c) %dopar% { # loop through all of your simulations - User needs to specify the max # of simulations (rows of parameters) in .csv
+  # require(R.utils) # package for sourceDirectory()
   
   # sourceDirectory(path=paste(getwd(),"/FUNCTIONS",sep=""),recursive=FALSE) # load all your functions
   
+  # I am not sure if this is neccasary. Can I just use totalsimuls?
   simulnumb <- i # assigns the simulation # from the for loop - will be used as an input to INPUT() to read the right row of .csv
   
-  INPUT2(simulnumb) # reads the .csv file of parameter values and assigns them to the global environment 
+  INPUT3(simulnumb) # reads the .csv file of parameter values and assigns them to the global environment 
   
-  SPECIES2(simulnumb) # function that builds the dataframe of species-specific parameters that is used in STEPX()
+  speciesmatrix <- SPECIES3(simulnumb) # function that builds the dataframe of species-specific parameters that is used in STEPX()
   
-  # define couple of things in the global environment that get used in STEPX() and OUTPUT()
+  # define couple of things in the environment that get used in STEPX() and OUTPUT()
   
   winters <- (timesteps+1) * seq(from=1, to=years, by=1) # ID timesteps that are winters - used in STEPX()
   
@@ -78,22 +88,28 @@ foreach (i=1:totalsimuls) %dopar% { # loop through all of your simulations - Use
   }
   
   LIST[[1]]<-START4() # Start the first time step with some individuals 
-
+  
   LIST<-STEP10() # Runs the model for all of the time steps - aging, senescence, reproduction, overwintering, movement, etc. 
   
-  OUTPUT2(threshold=70) # generates graphs - if you want .html animation you must specify animate=TRUE, set "FP regime" threshold here too 
+  OUTPUT3(threshold=70) # generates graphs - if you want .html animation you must specify animate=TRUE, set "FP regime" threshold here too 
   
-  RESULTS[simulnumb,1] <- propyears_avgFPcover_abovethreshold # assign the current simulations results to the correct spot
-  RESULTS[simulnumb,2] <- propyears_prop_daysFP_abovehalf # assign the current simulations results to the correct spot
+  # RESULTS[simulnumb,1] <- propyears_avgFPcover_abovethreshold # assign the current simulations results to the correct spot
+  # RESULTS[simulnumb,2] <- propyears_prop_daysFP_abovehalf # assign the current simulations results to the correct spot
+  # RESULTS[simulnumb,3] <- avg_avg_FPcover # assign the current simulations results to the correct spot  
+  
+  parameters$propyears_avgFPcover_abovethreshold[simulnumb] <- propyears_avgFPcover_abovethreshold
+  parameters$propyears_prop_daysFP_abovehalf[simulnumb] <- propyears_prop_daysFP_abovehalf
+  parameters$avg_avg_FPcover[simulnumb] <- avg_avg_FPcover
     
   # rm(list = ls()[!(ls() %in% c("RESULTS","parameters"))]) # clear workspace (except for RESULTS and parameters) for next simulation 
   
 }
 
 # add the results vectors to the original parameters data frame 
-parameters$propyears_avgFPcover_abovethreshold <- RESULTS[,1]
-parameters$propyears_prop_daysFP_abovehalf <- RESULTS[,2]
+# parameters$propyears_avgFPcover_abovethreshold <- RESULTS[,1]
+# parameters$propyears_prop_daysFP_abovehalf <- RESULTS[,2]
+# parameters$avg_avg_FPcover <- RESULTS[,3]
 
 # add these results to your original input file and write as a .csv 
-write.csv(parameters,"output04.csv",row.names=F) 
+write.csv(parameters,"output05.csv",row.names=F) 
 
