be6296b89168a1b27b45004eeb2f1a1eaf627d1d
mccannecology <michael.mccann@stonybrook.edu>
2014-04-22 19:45:33 -0400

    way too many issues but DEBUGGED!

--- FUNCTIONS/GROW_SAV.R
+++ FUNCTIONS/GROW_SAV.R
@@ -17,13 +17,13 @@ GROW_SAV <- function(x1,x2,x3,x4,x5) {
   for (j in 1:height) { # loop over all rows (height)
     for (k in 1:width) { # loop over all columns (width)
       if (x1[j,k] > 0) {
-        x2[j,k] <- x1[j,k] + (speciesmatrix$maxrgr[1])*x1[j,k] #* # initial biomass plus new growth 
+        x2[j,k] <- x1[j,k] + ((1-(x1[j,k]/100))*speciesmatrix$maxrgr[1])*x1[j,k] * # initial biomass plus new growth 
 
-                      #(1/(1+(lightlimitation*x1[j,k])+(shadingbyFP*x3[j,k])+lightattenuation)) * # limitation according to Scheffer et al. 2003 
-
-                      #((x4/(x4+speciesmatrix$halfsatP[1]))*(x5/(x5+speciesmatrix$halfsatN[1]))) - # nutrient limitation 
+                      (1/(1+(lightlimitation*x1[j,k])+(shadingbyFP*x3[j,k])+lightattenuation)) * # limitation according to Scheffer et al. 2003 
           
-                      #(lossSAV*x1[j,k]) # biomass loss 
+                      ((x4/(x4+speciesmatrix$halfsatP[1]))*(x5/(x5+speciesmatrix$halfsatN[1]))) - # nutrient limitation 
+          
+                      (lossSAV*x1[j,k]) # biomass loss 
       }
     }   
   }
