6279267754741cb6514288c2df17a508ec1f559d
mccannecology <michael.mccann@stonybrook.edu>
2014-06-20 08:56:19 -0400

    added some better plotting

--- Acessory FUNCTIONS/testing SAV and FP growth functions.R
+++ Acessory FUNCTIONS/testing SAV and FP growth functions.R
@@ -5,8 +5,8 @@ maxrgr <- 0.5
 halfsatP <- 0.2
 halfsatN <- 0.5 
 halfsatB <- 42 
-N <- 2 
-P <- 0.5 
+N <- 0.01 
+P <- N/4  
 loss <- 0.05
 
 FP_GROW <- function(x,y) {
@@ -16,10 +16,12 @@ FP_GROW <- function(x,y) {
 
 # This is linear 
 # There is no nutrient uptake
-temp<-0
+temp<-matrix(data=rep.int(0,1000),nrow=1000,ncol=2)
+temp[,1] <- seq(1,1000,1)
 for (i in 1:1000) {
   y <- FP_GROW(i)
-  temp <- c(temp, y)
+  temp[i,2] <- y
 }
-plot(temp)
+plot(temp[,1],temp[,2],ylab="New growth",xlab="Biomass_t")
+abline(a=0,b=1)
 
