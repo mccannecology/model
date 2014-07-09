168b70ce3481163d8ff26cfbd960e71c2ef2b6d6
mccannecology <michael.mccann@stonybrook.edu>
2014-07-09 13:55:53 -0400

    figuring out WIND() - so cells pile up when moving D and R

--- Acessory FUNCTIONS/wind_debugging.R
+++ Acessory FUNCTIONS/wind_debugging.R
@@ -19,83 +19,49 @@ direction_sample <- c(rep("U",prob_up*100),
                       rep("R",prob_right*100)) # make a vector of wind directions to sample from
 direction_of_wind <- sample(direction_sample,1) # sample from it 
 proptomove <- rnorm(1,wind_avg,wind_std) # assign an amount to move on the given time-step based on the average & SD of windiness 
-
 # alternatives to normal 
-proptomove <- rnorm(1,wind_avg,wind_std) # assign an amount to move on the given time-step based on the average & SD of windiness 
-
-hist(rnorm(1000,wind_avg,wind_std))
-
-jpeg("beta_dist_01.jpg",width=11,heigh=8,units="in",res=1600)
-par(mfrow=c(2,5))
-hist(rbeta(10000,5,5), sub=paste(c("mean = ", mean(rbeta(10000,5,5))), collapse = ""))
-hist(rbeta(10000,5,10), sub=paste(c("mean = ", mean(rbeta(10000,5,10))), collapse = ""))
-hist(rbeta(10000,5,20), sub=paste(c("mean = ", mean(rbeta(10000,5,20))), collapse = ""))
-hist(rbeta(10000,5,40), sub=paste(c("mean = ", mean(rbeta(10000,5,40))), collapse = ""))
-hist(rbeta(10000,5,80), sub=paste(c("mean = ", mean(rbeta(10000,5,80))), collapse = ""))
-
-hist(rbeta(10000,4,4), sub=paste(c("mean = ", mean(rbeta(10000,4,4))), collapse = ""))
-hist(rbeta(10000,4,8), sub=paste(c("mean = ", mean(rbeta(10000,4,8))), collapse = ""))
-hist(rbeta(10000,4,16), sub=paste(c("mean = ", mean(rbeta(10000,4,16))), collapse = ""))
-hist(rbeta(10000,4,32), sub=paste(c("mean = ", mean(rbeta(10000,4,32))), collapse = ""))
-hist(rbeta(10000,4,64), sub=paste(c("mean = ", mean(rbeta(10000,4,64))), collapse = ""))
-dev.off()
-
-jpeg("beta_dist_02.jpg",width=11,heigh=8,units="in",res=1600)
-par(mfrow=c(2,5))
-hist(rbeta(10000,3,3), sub=paste(c("mean = ", mean(rbeta(10000,3,3))), collapse = ""))
-hist(rbeta(10000,3,6), sub=paste(c("mean = ", mean(rbeta(10000,3,6))), collapse = ""))
-hist(rbeta(10000,3,12), sub=paste(c("mean = ", mean(rbeta(10000,3,12))), collapse = ""))
-hist(rbeta(10000,3,24), sub=paste(c("mean = ", mean(rbeta(10000,3,24))), collapse = ""))
-hist(rbeta(10000,3,48), sub=paste(c("mean = ", mean(rbeta(10000,3,48))), collapse = ""))
-
-hist(rbeta(10000,2,2), sub=paste(c("mean = ", mean(rbeta(10000,2,2))), collapse = ""))
-hist(rbeta(10000,2,4), sub=paste(c("mean = ", mean(rbeta(10000,2,4))), collapse = ""))
-hist(rbeta(10000,2,8), sub=paste(c("mean = ", mean(rbeta(10000,2,8))), collapse = ""))
-hist(rbeta(10000,2,16), sub=paste(c("mean = ", mean(rbeta(10000,2,16))), collapse = ""))
-hist(rbeta(10000,2,32), sub=paste(c("mean = ", mean(rbeta(10000,2,32))), collapse = ""))
-dev.off()
-
-jpeg("beta_dist_03.jpg",width=11,heigh=8,units="in",res=1600)
-par(mfrow=c(2,5))
-hist(rbeta(10000,1.5,1.5), sub=paste(c("mean = ", mean(rbeta(10000,1.5,1.5))), collapse = ""))
-hist(rbeta(10000,1.5,3), sub=paste(c("mean = ", mean(rbeta(10000,1.5,3))), collapse = ""))
-hist(rbeta(10000,1.5,6), sub=paste(c("mean = ", mean(rbeta(10000,1.5,6))), collapse = ""))
-hist(rbeta(10000,1.5,12), sub=paste(c("mean = ", mean(rbeta(10000,1.5,12))), collapse = ""))
-hist(rbeta(10000,1.5,24), sub=paste(c("mean = ", mean(rbeta(10000,1.5,24))), collapse = ""))
-
-hist(rbeta(10000,1,1), sub=paste(c("mean = ", mean(rbeta(10000,1,1))), collapse = ""))
-hist(rbeta(10000,1,2), sub=paste(c("mean = ", mean(rbeta(10000,1,2))), collapse = ""))
-hist(rbeta(10000,1,4), sub=paste(c("mean = ", mean(rbeta(10000,1,4))), collapse = ""))
-hist(rbeta(10000,1,8), sub=paste(c("mean = ", mean(rbeta(10000,1,8))), collapse = ""))
-hist(rbeta(10000,1,16), sub=paste(c("mean = ", mean(rbeta(10000,1,16))), collapse = ""))
-dev.off()
-
-jpeg("beta_dist_04.jpg",width=11,heigh=8,units="in",res=1600)
-par(mfrow=c(2,5))
-hist(rbeta(10000,0.5,0.5), sub=paste(c("mean = ", mean(rbeta(10000,0.5,0.5))), collapse = ""))
-hist(rbeta(10000,0.5,1), sub=paste(c("mean = ", mean(rbeta(10000,0.5,1))), collapse = ""))
-hist(rbeta(10000,0.5,2), sub=paste(c("mean = ", mean(rbeta(10000,0.5,2))), collapse = ""))
-hist(rbeta(10000,0.5,4), sub=paste(c("mean = ", mean(rbeta(10000,0.5,4))), collapse = ""))
-hist(rbeta(10000,0.5,8), sub=paste(c("mean = ", mean(rbeta(10000,0.5,8))), collapse = ""))
-
-hist(rbeta(10000,0.1,0.1), sub=paste(c("mean = ", mean(rbeta(10000,0.1,0.1))), collapse = ""))
-hist(rbeta(10000,0.1,0.2), sub=paste(c("mean = ", mean(rbeta(10000,0.1,0.2))), collapse = ""))
-hist(rbeta(10000,0.1,0.4), sub=paste(c("mean = ", mean(rbeta(10000,0.1,0.4))), collapse = ""))
-hist(rbeta(10000,0.1,0.8), sub=paste(c("mean = ", mean(rbeta(10000,0.1,0.8))), collapse = ""))
-hist(rbeta(10000,0.1,1.6), sub=paste(c("mean = ", mean(rbeta(10000,0.1,1.6))), collapse = ""))
-dev.off()
+proptomove <- rbeta(1,wind_shape1,wind_shape2) # assign an amount to move on the given time-step based on a beta distribution
 
 if (proptomove < 0) {proptomove <- 0} # make any negative values 0 
 if (proptomove > 1) {proptomove <- 1} # cannot move >100% of a cell  
 
 nextstep$FP <- lapply(nextstep$FP,function(x){
-  WIND21(x,proptomove,direction_of_wind,full_threshold=800) 
+  WIND21(x,proptomove,direction_of_wind,full_thres_wind=800) 
 })
+
+direction_of_wind <- "U"
+direction_of_wind <- "D"
+direction_of_wind <- "L"
+direction_of_wind <- "R"
+proptomove <- 1 
+proptomove <- 0.99
+proptomove <- 0.55
+proptomove <- 0.75
+proptomove <- 0.95
+proptomove <- 0.1
+
+a <- LIST[[1]]$FP[[1]]
+
+b <- WIND22(a,proptomove,direction_of_wind,full_thresh_wind=800) 
+plot(stack(raster(a),raster(b)))
+a <- b 
 
 
 a <- LIST[[1]]$FP[[1]]
 
-b <- WIND21(a,proptomove,direction_of_wind,full_threshold=800) 
+b <- WIND22(a,proptomove,direction_of_wind,full_thresh_wind=800) 
 plot(stack(raster(a),raster(b)))
 a <- b 
 
+
+
+
+x1 <- LIST[[1]]$FP[[1]]
+
+
+
+for (j in 1:height) { # loop over all rows (height)
+  for (k in 1:width) { # loop over all columns (width)
+    print(c(j,k))
+  }
+}
--- FUNCTIONS/WIND22.R
+++ FUNCTIONS/WIND22.R
@@ -5,11 +5,47 @@
 # x1... LIST[[i]]$FPmatrix            #
 #                                     #
 # Created: MJ McCann 5/8/2014         #
-# Updated: 07/08/2014
+# Updated: 07/08/2014                 #
 #######################################
 
-WIND21 <- function (x1,proptomove,direction_of_wind,full_thresh_wind=800) {
+WIND22 <- function (x1,proptomove,direction_of_wind,full_thresh_wind=800) {
   x2 <- x1 # this will hold the values for the next time step 
+  
+  # new mass originating from center cells 
+  new_mass_C <- matrix(0,nrow=height,ncol=width)
+  remove_mass_C <- matrix(0,nrow=height,ncol=width)
+  
+  # new mass originating from 
+  new_mass_Trow <- new_mass_C
+  remove_mass_Trow <- new_mass_C
+
+  # new mass originating from 
+  new_mass_Brow <- new_mass_C
+  remove_mass_Brow <- new_mass_C
+  
+  # new mass originating from 
+  new_mass_Fcol <- new_mass_C
+  remove_mass_Fcol <- new_mass_C
+  
+  # new mass originating from 
+  new_mass_Lcol <- new_mass_C
+  remove_mass_Lcol <- new_mass_C
+  
+  # new mass originating from 
+  new_mass_TL <- new_mass_C
+  remove_mass_TL <- new_mass_C
+  
+  # new mass originating from 
+  new_mass_TR <- new_mass_C
+  remove_mass_TR <- new_mass_C
+  
+  # new mass originating from 
+  new_mass_BL <- new_mass_C
+  remove_mass_BL <- new_mass_C
+  
+  # new mass originating from 
+  new_mass_BR <- new_mass_C
+  remove_mass_BR <- new_mass_C
   
   for (j in 1:height) { # loop over all rows (height)
     for (k in 1:width) { # loop over all columns (width)
@@ -19,31 +55,31 @@ WIND21 <- function (x1,proptomove,direction_of_wind,full_thresh_wind=800) {
         
         # if the cell is in the center, then do the following: 
         if ((j>=2 & j<=(height-1)) & (k>=2 & k<=(width-1))) {                                           
-                
+          
           amounttomove <- x1[j,k]*proptomove # amount of cell biomass * proportion of occupied cell biomass that will move 
           
           if (direction_of_wind == "U"){  
             if (x1[j-1,k] < full_thresh_wind) { # only move if the destination cell is not "too full" 
-              x2[j-1,k] <- x1[j-1,k] + amounttomove # add amountomove UP
-              x2[j,k] <- x1[j,k] - amounttomove # subtract this value from the original spot 
+              new_mass_C[j-1,k] <- amounttomove # add amountomove UP
+              remove_mass_C[j,k] <- -amounttomove # subtract this value from the original spot 
             }
           }
           if (direction_of_wind == "R"){    
             if (x1[j,k+1] < full_thresh_wind) { # only move if the destination cell is not "too full" 
-            x2[j,k+1] <- x1[j,k+1] + amounttomove # add amountomove RIGHT
-            x2[j,k] <- x1[j,k] - amounttomove # subtract this value from the original spot 
+              new_mass_C[j,k+1] <- amounttomove # add amountomove RIGHT
+              remove_mass_C[j,k] <- -amounttomove # subtract this value from the original spot 
             }
           }
           if (direction_of_wind == "D"){
             if (x1[j+1,k] < full_thresh_wind) { # only move if the destination cell is not "too full" 
-              x2[j+1,k] <- x1[j+1,k] + amounttomove # add amountomove DOWN
-              x2[j,k] <- x1[j,k] - amounttomove # subtract this value from the original spot 
+              new_mass_C[j+1,k] <- amounttomove # add amountomove DOWN
+              remove_mass_C[j,k] <- -amounttomove # subtract this value from the original spot 
             }
           }
           if (direction_of_wind == "L"){
             if (x1[j,k-1] < full_thresh_wind) { # only move if the destination cell is not "too full" 
-              x2[j,k-1] <- x1[j,k-1] + amounttomove # add amountomove LEFT
-              x2[j,k] <- x1[j,k] - amounttomove # subtract this value from the original spot 
+              new_mass_C[j,k-1] <- amounttomove # add amountomove LEFT
+              remove_mass_C[j,k] <- -amounttomove # subtract this value from the original spot 
             }
           }
           
@@ -54,25 +90,26 @@ WIND21 <- function (x1,proptomove,direction_of_wind,full_thresh_wind=800) {
           
           amounttomove <- x1[j,k] * proptomove # amount of cell biomass * proportion of occupied cell biomass that will move 
           
-          #if (direction_of_wind == "U"){       
-          #  x1[j,k] <- x1[j,k] 
-          #}
+          if (direction_of_wind == "U"){       
+            new_mass_Trow[j,k] <- 0 
+            remove_mass_Trow[j,k] <- 0
+          }
           if (direction_of_wind == "R"){    
             if (x1[j,k+1] < full_thresh_wind) { # only move if the destination cell is not "too full" 
-              x2[j,k+1] <- x1[j,k+1] + amounttomove # add amountomove RIGHT
-              x2[j,k] <- x1[j,k] - amounttomove # subtract this value from the original spot 
+              new_mass_Trow[j,k+1] <- amounttomove # add amountomove RIGHT
+              remove_mass_Trow[j,k] <- -amounttomove # subtract this value from the original spot 
             }
           }
           if (direction_of_wind == "D"){
             if (x1[j+1,k] < full_thresh_wind) { # only move if the destination cell is not "too full" 
-              x2[j+1,k] <- x1[j+1,k] + amounttomove # add amountomove DOWN
-              x2[j,k] <- x1[j,k] - amounttomove # subtract this value from the original spot 
+              new_mass_Trow[j+1,k] <- amounttomove # add amountomove DOWN
+              remove_mass_Trow[j,k] <- -amounttomove # subtract this value from the original spot 
             }
           }
           if (direction_of_wind == "L"){
             if (x1[j,k-1] < full_thresh_wind) { # only move if the destination cell is not "too full" 
-              x2[j,k-1] <- x1[j,k-1] + amounttomove # add amountomove LEFT
-              x2[j,k] <- x1[j,k] - amounttomove # subtract this value from the original spot 
+              new_mass_Trow[j,k-1] <- amounttomove # add amountomove LEFT
+              remove_mass_Trow[j,k] <- -amounttomove # subtract this value from the original spot 
             }
           }
         } # closes  if loop - through occupied top row 
@@ -84,23 +121,24 @@ WIND21 <- function (x1,proptomove,direction_of_wind,full_thresh_wind=800) {
           
           if (direction_of_wind == "U"){  
             if (x1[j-1,k] < full_thresh_wind) { # only move if the destination cell is not "too full" 
-              x2[j-1,k] <- x1[j-1,k] + amounttomove # add amountomove UP
-              x2[j,k] <- x1[j,k] - amounttomove # subtract this value from the original spot 
+              new_mass_Brow[j-1,k] <- amounttomove # add amountomove UP
+              remove_mass_Brow[j,k] <- -amounttomove # subtract this value from the original spot 
             }
           }
           if (direction_of_wind == "R"){    
             if (x1[j,k+1] < full_thresh_wind) { # only move if the destination cell is not "too full" 
-              x2[j,k+1] <- x1[j,k+1] + amounttomove # add amountomove RIGHT
-              x2[j,k] <- x1[j,k] - amounttomove # subtract this value from the original spot 
+              new_mass_Brow[j,k+1] <- amounttomove # add amountomove RIGHT
+              remove_mass_Brow[j,k] <- -amounttomove # subtract this value from the original spot 
             }
           }
-          #if (direction_of_wind == "D"){       
-          #  x2[j,k] <- x1[j,k] 
-          #}
+          if (direction_of_wind == "D"){       
+            new_mass_Brow[j,k] <- 0 
+            remove_mass_Brow[j,k] <- 0
+          }
           if (direction_of_wind == "L"){
             if (x1[j,k-1] < full_thresh_wind) { # only move if the destination cell is not "too full" 
-              x2[j,k-1] <- x1[j,k-1] + amounttomove # add amountomove LEFT
-              x2[j,k] <- x1[j,k] - amounttomove # subtract this value from the original spot 
+              new_mass_Brow[j,k-1] <- amounttomove # add amountomove LEFT
+              remove_mass_Brow[j,k] <- -amounttomove # subtract this value from the original spot 
             }
           }
         } # closes  if loop - through occupied bottom row 
@@ -112,25 +150,26 @@ WIND21 <- function (x1,proptomove,direction_of_wind,full_thresh_wind=800) {
           
           if (direction_of_wind == "U"){  
             if (x1[j-1,k] < full_thresh_wind) { # only move if the destination cell is not "too full" 
-              x2[j-1,k] <- x1[j-1,k] + amounttomove # add amountomove UP
-              x2[j,k] <- x1[j,k] - amounttomove # subtract this value from the original spot 
+              new_mass_Fcol[j-1,k] <- amounttomove # add amountomove UP
+              remove_mass_Fcol[j,k] <- -amounttomove # subtract this value from the original spot 
             }
           }
           if (direction_of_wind == "R"){    
             if (x1[j,k+1] < full_thresh_wind) { # only move if the destination cell is not "too full" 
-              x2[j,k+1] <- x1[j,k+1] + amounttomove # add amountomove RIGHT
-              x2[j,k] <- x1[j,k] - amounttomove # subtract this value from the original spot 
+              new_mass_Fcol[j,k+1] <- amounttomove # add amountomove RIGHT
+              remove_mass_Fcol[j,k] <- -amounttomove # subtract this value from the original spot 
             }
           }
           if (direction_of_wind == "D"){
             if (x1[j+1,k] < full_thresh_wind) { # only move if the destination cell is not "too full" 
-              x2[j+1,k] <- x1[j+1,k] + amounttomove # add amountomove DOWN
-              x2[j,k] <- x1[j,k] - amounttomove # subtract this value from the original spot 
+              new_mass_Fcol[j+1,k] <- amounttomove # add amountomove DOWN
+              remove_mass_Fcol[j,k] <- -amounttomove # subtract this value from the original spot 
             }
           }
-          #if (direction_of_wind == "L"){       
-          #  x2[j,k] <- x1[j,k] 
-          #}
+          if (direction_of_wind == "L"){       
+            new_mass_Fcol[j,k] <- 0 
+            remove_mass_Fcol[j,k] <- 0
+          }
         } # closes  if loop - through occupied 1st column  
         
         # if the cell is in the last column (but not a corner), then do the following: 
@@ -140,23 +179,24 @@ WIND21 <- function (x1,proptomove,direction_of_wind,full_thresh_wind=800) {
           
           if (direction_of_wind == "U"){  
             if (x1[j-1,k] < full_thresh_wind) { # only move if the destination cell is not "too full" 
-              x2[j-1,k] <- x1[j-1,k] + amounttomove # add amountomove UP
-              x2[j,k] <- x1[j,k] - amounttomove # subtract this value from the original spot 
+              new_mass_Lcol[j-1,k] <- amounttomove # add amountomove UP
+              remove_mass_Lcol[j,k] <- -amounttomove # subtract this value from the original spot 
             }
           }
-          #if (direction_of_wind == "R"){       
-          #  x2[j,k] <- x1[j,k] 
-          #}
+          if (direction_of_wind == "R"){       
+            new_mass_Lcol[j,k] <- 0 
+            remove_mass_Lcol[j,k] <- 0
+          }
           if (direction_of_wind == "D"){
             if (x1[j+1,k] < full_thresh_wind) { # only move if the destination cell is not "too full" 
-              x2[j+1,k] <- x1[j+1,k] + amounttomove # add amountomove DOWN
-              x2[j,k] <- x1[j,k] - amounttomove # subtract this value from the original spot 
+              new_mass_Lcol[j+1,k] <- amounttomove # add amountomove DOWN
+              remove_mass_Lcol[j,k] <- -amounttomove # subtract this value from the original spot 
             }
           }
           if (direction_of_wind == "L"){
             if (x1[j,k-1] < full_thresh_wind) { # only move if the destination cell is not "too full" 
-              x2[j,k-1] <- x1[j,k-1] + amounttomove # add amountomove LEFT
-              x2[j,k] <- x1[j,k] - amounttomove # subtract this value from the original spot 
+              new_mass_Lcol[j,k-1] <- amounttomove # add amountomove LEFT
+              remove_mass_Lcol[j,k] <- -amounttomove # subtract this value from the original spot 
             }
           }
         } # closes  if loop - through occupied last column 
@@ -166,24 +206,26 @@ WIND21 <- function (x1,proptomove,direction_of_wind,full_thresh_wind=800) {
           
           amounttomove <- x1[j,k] * proptomove # amount of cell biomass * proportion of occupied cell biomass that will move 
           
-          #if (direction_of_wind == "U"){        
-          #  x1[j,k] <- x1[j,k] 
-          #}
+          if (direction_of_wind == "U"){        
+            new_mass_TL[j,k] <- 0 
+            remove_mass_TL[j,k] <- 0
+          }
           if (direction_of_wind == "R"){    
             if (x1[j,k+1] < full_thresh_wind) { # only move if the destination cell is not "too full" 
-              x2[j,k+1] <- x1[j,k+1] + amounttomove # add amountomove RIGHT
-              x2[j,k] <- x1[j,k] - amounttomove # subtract this value from the original spot 
+              new_mass_TL[j,k+1] <- amounttomove # add amountomove RIGHT
+              remove_mass_TL[j,k] <- -amounttomove # subtract this value from the original spot 
             }
           }
           if (direction_of_wind == "D"){
             if (x1[j+1,k] < full_thresh_wind) { # only move if the destination cell is not "too full" 
-              x2[j+1,k] <- x1[j+1,k] + amounttomove # add amountomove DOWN
-              x2[j,k] <- x1[j,k] - amounttomove # subtract this value from the original spot 
+              new_mass_TL[j+1,k] <- amounttomove # add amountomove DOWN
+              remove_mass_TL[j,k] <- -amounttomove # subtract this value from the original spot 
             }
           }
-          #if (direction_of_wind == "L"){       
-          #  x2[j,k] <- x1[j,k] 
-          #}  
+          if (direction_of_wind == "L"){       
+            new_mass_TL[j,k] <- 0 
+            remove_mass_TL[j,k] <- 0
+          }  
         } # closes  if loop - through occupied top left
         
         # if the cell is in the top right corner, then do the following:  
@@ -191,22 +233,24 @@ WIND21 <- function (x1,proptomove,direction_of_wind,full_thresh_wind=800) {
           
           amounttomove <- x1[j,k] * proptomove # amount of cell biomass * proportion of occupied cell biomass that will move 
           
-          #if (direction_of_wind == "U"){       
-          #  x2[j,k] <- x1[j,k] 
-          #}
-          #if (direction_of_wind == "R"){       
-          #  x2[j,k] <- x1[j,k] 
-          #}
+          if (direction_of_wind == "U"){       
+            new_mass_TR[j,k] <- 0 
+            remove_mass_TR[j,k] <- 0
+          }
+          if (direction_of_wind == "R"){       
+            new_mass_TR[j,k] <- 0 
+            remove_mass_TR[j,k] <- 0
+          }
           if (direction_of_wind == "D"){
             if (x1[j+1,k] < full_thresh_wind) { # only move if the destination cell is not "too full" 
-              x2[j+1,k] <- x1[j+1,k] + amounttomove # add amountomove DOWN
-              x2[j,k] <- x1[j,k] - amounttomove # subtract this value from the original spot 
+              new_mass_TR[j+1,k] <- amounttomove # add amountomove DOWN
+              remove_mass_TR[j,k] <- -amounttomove # subtract this value from the original spot 
             }
           }
           if (direction_of_wind == "L"){
             if (x1[j,k-1] < full_thresh_wind) { # only move if the destination cell is not "too full" 
-              x2[j,k-1] <- x1[j,k-1] + amounttomove # add amountomove LEFT
-              x2[j,k] <- x1[j,k] - amounttomove # subtract this value from the original spot 
+              new_mass_TR[j,k-1] <- amounttomove # add amountomove LEFT
+              remove_mass_TR[j,k] <- -amounttomove # subtract this value from the original spot 
             }
           }
           
@@ -219,23 +263,24 @@ WIND21 <- function (x1,proptomove,direction_of_wind,full_thresh_wind=800) {
           
           if (direction_of_wind == "U"){  
             if (x1[j-1,k] < full_thresh_wind) { # only move if the destination cell is not "too full" 
-              x2[j-1,k] <- x1[j-1,k] + amounttomove # add amountomove UP
-              x2[j,k] <- x1[j,k] - amounttomove # subtract this value from the original spot 
+              new_mass_BL[j-1,k] <- amounttomove # add amountomove UP
+              remove_mass_BL[j,k] <- -amounttomove # subtract this value from the original spot 
             }
           }
           if (direction_of_wind == "R"){    
             if (x1[j,k+1] < full_thresh_wind) { # only move if the destination cell is not "too full" 
-              x2[j,k+1] <- x1[j,k+1] + amounttomove # add amountomove RIGHT
-              x2[j,k] <- x1[j,k] - amounttomove # subtract this value from the original spot 
+              new_mass_BL[j,k+1] <- amounttomove # add amountomove RIGHT
+              remove_mass_BL[j,k] <- -amounttomove # subtract this value from the original spot 
             }
           }
-          #if (direction_of_wind == "D"){      
-          #  x2[j,k] <- x1[j,k] 
-          #}
-          #if (direction_of_wind == "L"){       
-          #  x2[j,k] <- x1[j,k] 
-          #}
-          
+          if (direction_of_wind == "D"){      
+            new_mass_BL[j,k] <- 0 
+            remove_mass_BL[j,k] <- 0
+          }
+          if (direction_of_wind == "L"){       
+            new_mass_BL[j,k] <- 0 
+            remove_mass_BL[j,k] <- 0
+          }
         } # closes  if loop - through occupied bottom left 
         
         # if the cell is in the bottom right corner, then do the following:  
@@ -245,20 +290,22 @@ WIND21 <- function (x1,proptomove,direction_of_wind,full_thresh_wind=800) {
           
           if (direction_of_wind == "U"){  
             if (x1[j-1,k] < full_thresh_wind) { # only move if the destination cell is not "too full" 
-              x2[j-1,k] <- x1[j-1,k] + amounttomove # add amountomove UP
-              x2[j,k] <- x1[j,k] - amounttomove # subtract this value from the original spot 
+              new_mass_BR[j-1,k] <- amounttomove # add amountomove UP
+              remove_mass[j,k] <- -amounttomove # subtract this value from the original spot 
             }
           }
-          #if (direction_of_wind == "R"){       
-          #  x2[j,k] <- x1[j,k]
-          #}
-          #if (direction_of_wind == "D"){      
-          #  x2[j,k] <- x1[j,k] 
-          #}
+          if (direction_of_wind == "R"){       
+            new_mass_BR[j,k] <- 0 
+            remove_mass_BR[j,k] <- 0
+          }
+          if (direction_of_wind == "D"){      
+            new_mass_BR[j,k] <- 0 
+            remove_mass_BR[j,k] <- 0
+          }
           if (direction_of_wind == "L"){
             if (x1[j,k-1] < full_thresh_wind) { # only move if the destination cell is not "too full" 
-              x2[j,k-1] <- x1[j,k-1] + amounttomove # add amountomove LEFT
-              x2[j,k] <- x1[j,k] - amounttomove # subtract this value from the original spot 
+              new_mass_BR[j,k-1] <-  amounttomove # add amountomove LEFT
+              remove_mass_BR[j,k] <- -amounttomove # subtract this value from the original spot 
             }
           }
         } # closes  if loop - through occupied bottom right  
@@ -266,5 +313,16 @@ WIND21 <- function (x1,proptomove,direction_of_wind,full_thresh_wind=800) {
       } # closes if loop - through occupied cells 
     } # closes for loop - through width/columns (k)    
   } # closes for loop - through height/rows (j)  
+  
+  x2 <- x1 + new_mass_C + remove_mass_C +
+    new_mass_Trow + remove_mass_Trow +
+    new_mass_Brow + remove_mass_Brow +
+    new_mass_Fcol + remove_mass_Fcol +
+    new_mass_Lcol + remove_mass_Lcol +
+    new_mass_TL + remove_mass_TL +
+    new_mass_TR + remove_mass_TR +
+    new_mass_BL + remove_mass_BL +
+    new_mass_BR + remove_mass_BR 
+    
   return(x2)
 }
