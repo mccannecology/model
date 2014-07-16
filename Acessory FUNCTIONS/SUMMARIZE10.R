b1aa7cd3a074206c5117d789d1124dba48b2dd44
mccannecology <michael.mccann@stonybrook.edu>
2014-07-16 09:04:16 -0400

    summarizing simulation results

--- Acessory FUNCTIONS/SUMMARIZE10.R
+++ Acessory FUNCTIONS/SUMMARIZE10.R
@@ -46,3 +46,48 @@ SAV_plot <- SAV_plot + theme_bw(base_size=18)
 SAV_plot
 ggsave(file="output22 - FP&SAV - lots of changes - SAV.pdf",SAV_plot, height=8,width=11)
 
+
+
+
+########################################
+########################################
+# initial conditions were 1:1 or 50:50 #
+########################################
+########################################
+data_2 <- subset(data, data$initial_perc_SAV_cover==1 & data$initial_perc_FP_cover==1 |
+                   data$initial_perc_SAV_cover==50 & data$initial_perc_FP_cover==50)
+head(data_2)
+nrow(data_2)
+
+
+#########################
+# Y = average average FP cover for all years (except for first three)
+FP_plot_2 <- ggplot(data_2, aes(x=TOTALN,y=avg_avg_FPcover,
+                            colour=as.factor(shadingbyFP),
+                            shape=as.factor(shadingbyFP))) 
+FP_plot_2 <- FP_plot_2 + scale_colour_grey()
+FP_plot_2 <- FP_plot_2 + geom_point(position="jitter",size=3)
+FP_plot_2 <- FP_plot_2 + facet_grid(lightlimitationSAV ~ lightlimitation01)
+FP_plot_2 <- FP_plot_2 + ylim(0,101)
+FP_plot_2 <- FP_plot_2 + xlab("Total N (mg/L)")
+FP_plot_2 <- FP_plot_2 + ylab(expression(paste("Annual avg. FP cover excl. 1st 3 yr")))
+FP_plot_2 <- FP_plot_2 + labs(colour=expression(paste("shadingbyFP")))
+FP_plot_2 <- FP_plot_2 + labs(shape=expression(paste("shadingbyFP")))
+FP_plot_2 <- FP_plot_2 + theme_bw(base_size=18)
+FP_plot_2
+ggsave(file="output22 - FP&SAV - lots of changes - FP - 2.pdf",FP_plot_2, height=8,width=11)
+
+SAV_plot_2 <- ggplot(data_2, aes(x=TOTALN,y=avg_avg_SAVcover,
+                             colour=as.factor(shadingbyFP),
+                             shape=as.factor(shadingbyFP))) 
+SAV_plot_2 <- SAV_plot_2 + scale_colour_grey()
+SAV_plot_2 <- SAV_plot_2 + geom_point(position="jitter",size=3)
+SAV_plot_2 <- SAV_plot_2 + facet_grid(lightlimitationSAV ~ lightlimitation01)
+#SAV_plot_2 <- SAV_plot_2 + ylim(0,101)
+SAV_plot_2 <- SAV_plot_2 + xlab("Total N (mg/L)")
+SAV_plot_2 <- SAV_plot_2 + ylab(expression(paste("Annual avg. SAV cover excl. 1st 3 yr")))
+SAV_plot_2 <- SAV_plot_2 + labs(colour=expression(paste("shadingbyFP")))
+SAV_plot_2 <- SAV_plot_2 + labs(shape=expression(paste("shadingbyFP")))
+SAV_plot_2 <- SAV_plot_2 + theme_bw(base_size=18)
+SAV_plot_2
+ggsave(file="output22 - FP&SAV - lots of changes - SAV - 2.pdf",SAV_plot_2, height=8,width=11)
