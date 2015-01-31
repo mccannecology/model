247a305077c685965b4ccc908c18afb5207ca542
mccannecology <mccannmikejames@gmail.com>
2015-01-31 15:19:06 -0500

    Testing Janse & van Puijenbroek uptake

--- Acessory FUNCTIONS/testing_Janse_vanPuijenbroek_nutrien_uptake.R
+++ Acessory FUNCTIONS/testing_Janse_vanPuijenbroek_nutrien_uptake.R
@@ -1,20 +1,21 @@
-temp <- 20
-
 ##################
 # Values for SAV #
 ##################
-cVNUptMax <- 0.0385 
-rND <- 0.03
-cNDmin <- 0.0125 
-cNDmax <- 0.04
-cQ10Prod <- 1.75
-cAffNUpt <- 0.011 
+# I used the median between the min. and max. specified by Janse & van Puijenbroek 1997
+temp <- 20 # temperature 
+cVNUptMax <- 0.0385 # max uptake rate 
+#rND <- 0.03 # current ratio of nitrogen to plant dry mass
+cNDmin <- 0.0125  # min. ratio of nitrogen to plant dry mass
+cNDmax <- 0.04 # max. ratio of nitrogen to plant dry mass
+cQ10Prod <- 1.75 # factor by which growth rate increases due to a 10C increase in temp 
+cAffNUpt <- 0.011 # affinity for N [m3/gD/d]
 
 #################
 # Values for FP #
 #################
+temp <- 20
 cVNUptMax <- 0.0385 
-rND <- 0.03
+#rND <- 0.03
 cNDmin <- 0.02
 cNDmax <- 0.75
 cQ10Prod <- 2.25
@@ -23,7 +24,7 @@ cAffNUpt <- 0.011
 ##################### 
 # Define a function # 
 ##################### 
-uptake_rate <- function(TOTALN){
+uptake_rate <- function(TOTALN, rND){
   # corrected maximum uptake rate 
   uptake_max_cor <- cVNUptMax*cQ10Prod^(0.1*(temp-20))*((cNDmax-rND)/(cNDmax-cNDmin))
   
@@ -36,19 +37,32 @@ uptake_rate <- function(TOTALN){
 ######################### 
 # Test function for SAV # 
 ######################### 
-mydata_SAV <- matrix(nrow = 1, ncol = 2)
+
+# for new biomass
+mydata_SAV_new <- matrix(nrow = 1, ncol = 2)
 
 cVNUptMax <- 0.0385 
-rND <- 0.03
 cNDmin <- 0.0125 
 cNDmax <- 0.04
 cQ10Prod <- 1.75
 cAffNUpt <- 0.011 
 
 for(j in seq(0,10,0.5)){
-  mydata_SAV <- rbind(mydata_SAV, c(j,uptake_rate(j)))
+  mydata_SAV_new <- rbind(mydata_SAV_new, c(j,uptake_rate(TOTALN=j,rND=0.0125)))
 }
 
+# for old biomass
+mydata_SAV_old <- matrix(nrow = 1, ncol = 2)
+
+cVNUptMax <- 0.0385 
+cNDmin <- 0.0125 
+cNDmax <- 0.04
+cQ10Prod <- 1.75
+cAffNUpt <- 0.011 
+
+for(j in seq(0,10,0.5)){
+  mydata_SAV_old <- rbind(mydata_SAV_old, c(j,uptake_rate(TOTALN=j,rND=0.03)))
+}
 
 ######################## 
 # Test function for FP # 
