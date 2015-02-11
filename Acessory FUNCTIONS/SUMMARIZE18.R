3ef852721fdea3ea0786348a3380d1abc03fd698
mccannecology <mccannmikejames@gmail.com>
2015-02-11 17:24:56 -0500

    Fixed plot of growing season

--- Acessory FUNCTIONS/SUMMARIZE18.R
+++ Acessory FUNCTIONS/SUMMARIZE18.R
@@ -30,7 +30,7 @@ data$state_score <- (data$FP_end_yr04 - data$SAV_end_yr04) / sqrt(2)
 # change "data$days" to a factor with different levels 
 data$days_factor <- as.factor(data$days)
 levels(data$days_factor) <- c("12 years\n 50 days per year","8 years\n 75 days per year",
-                              "5 years\n 150 days per year","1 year\n 600 days per year")
+                              "4 years\n 150 days per year","1 year\n 600 days per year")
 
 # Plot 
 SAV_FP_plot <- ggplot(data, aes(x=TOTALN,y=state_score))
@@ -40,7 +40,7 @@ SAV_FP_plot <- SAV_FP_plot + facet_grid(. ~ days_factor)
 SAV_FP_plot <- SAV_FP_plot + xlab("Total N (mg/L)")
 SAV_FP_plot <- SAV_FP_plot + ylab(expression(paste("Plant state score")))
 SAV_FP_plot <- SAV_FP_plot + theme_bw(base_size=18)
-SAV_FP_plot <- SAV_FP_plot + ggtitle("1 ha, rectangle")
+#SAV_FP_plot <- SAV_FP_plot + ggtitle("1 ha, rectangle")
 SAV_FP_plot
 
 ggsave(file="output_growing_season_state_score.jpg",SAV_FP_plot, height=4,width=12)
